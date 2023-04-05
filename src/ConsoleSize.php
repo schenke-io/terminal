1f99b51f50ab0690d012a88e3ee9ee8e1be1465a
Schenke-IO <kschenke@schenke.io>
2023-04-05 18:47:28 +0200

    adding ConsoleSize

---
+++ src/ConsoleSize.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SchenkeIo\Terminal;
+
+use Symfony\Component\Console;
+
+class ConsoleSize
+{
+    public static function width(): int
+    {
+        return (new Console\Terminal())->getWidth();
+    }
+
+    public static function height(): int
+    {
+        return (new Console\Terminal())->getHeight();
+    }
+
+}
